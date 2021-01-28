af256550d99cd62a7994bcb9d4b6c89fd4c5c151
Collin Chapman <cchap14@gmail.com>
2021-01-27 19:59:13 -0800

    create a third if statement to alert user if they are exactly 21 years old

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,28 @@
+
 $(document).ready(function() {
   const age = parseInt(prompt("How old are you?"));
 
-  if (age >= 21) {
+  if (age > 21) {
+    $("#drinks").show();
+  } else if (age === 21) {
+    alert("Now don't go wild!");
     $("#drinks").show();
   } else {
     $("#under-21").show();
   }
-})
+});
+
+//test code to understand boolean if statements
+/*
+
+$(document).ready(function() {
+  const over21 = confirm("are you over 21? Click OK for yes or Cancel for no.");
+
+  if (over21) {
+    $("#drinks").show();
+  } else {
+    $("#under-21").show();
+  }
+});
+
+*/
